51f0ff0ec009fee52167ea92492d69abdfbd234c
KibuuleNoah <noahkibuule3@gmail.com>
2023-12-20 09:49:34 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# websoctest
+# websoctest
+# websoctest
---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+@app.route("/chat")
+def index():
+    return render_template("index.html")
+
+
+@socketio.on("message")
+def handle_message(msg):
+    print("Message: " + msg)
+    socketio.emit("message", msg)
+
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
